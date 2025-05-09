a050f3e6fb5191b05268a6fe189dae8a0c2daeec
JohnnyCena123 <iamahuman1395@gmail.com>
2025-05-09 15:13:26 +0300

    its pretty much done

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,237 @@
+#include <array>
+#include <format>
+#include <iostream>
 #include <raylib.h>
+#include <raymath.h>
+#include <rlgl.h>
+#include <vcruntime.h>
 
-int main() {
 
-	int constexpr screenWidth = 1800;
-	int constexpr screenHeight = 900;
+/*
+ * Constants
+ */
+float constexpr SQUARE_SIZE = 200.f;
+float constexpr Y_FREE_SPACE = 75.f;
+int constexpr SCREEN_WIDTH = 6 * SQUARE_SIZE;
+int constexpr SCREEN_HEIGHT = 3 * SQUARE_SIZE + Y_FREE_SPACE * 2;
+Vector2 constexpr BOARD_START = {SQUARE_SIZE * 1.5f, Y_FREE_SPACE};
+Color constexpr SQUARE_COLOR = { 155, 228, 255, 115 };
+	 
 
-	InitWindow(screenWidth, screenHeight, "Hello!");
+using Player = enum { None, X, O };
 
-	SetTargetFPS(60);
+std::array<std::array<std::pair<Rectangle, Player>, 3>, 3> squares = {};
 
+void draw() {
+
+	DrawLineV(
+		{SQUARE_SIZE + BOARD_START.x, BOARD_START.y},
+		{SQUARE_SIZE + BOARD_START.x, SQUARE_SIZE * 3.f + BOARD_START.y}, BLACK
+	); // left line
+	DrawLineV(
+		{SQUARE_SIZE * 2 + BOARD_START.x, BOARD_START.y},
+		{SQUARE_SIZE * 2 + BOARD_START.x, SQUARE_SIZE * 3.f + BOARD_START.y}, BLACK
+	); // right line
+
+	DrawLineV(
+		{BOARD_START.x, SQUARE_SIZE + BOARD_START.y},
+		{SQUARE_SIZE * 4.5f, SQUARE_SIZE + BOARD_START.y}, BLACK
+	); // top line
+	DrawLineV(
+		{BOARD_START.x, SQUARE_SIZE * 2 + BOARD_START.y},
+		{SQUARE_SIZE * 3 + BOARD_START.x, SQUARE_SIZE * 2 + BOARD_START.y}, BLACK
+	); // bottom line
+
+	for (auto& line : squares) for (auto& [rect, item] : line) {
+		DrawRectangleRec(rect, SQUARE_COLOR);
+		switch (item) {
+			case None: break;
+			case X: 
+				DrawLineEx(
+					{rect.x + 10.f, rect.y + 10.f}, {
+						rect.x + rect.width - 10.f, 
+						rect.y + rect.height - 10.f
+					}, 3.f, BLACK
+				);
+				DrawLineEx(
+					{rect.x + rect.width - 10.f, rect.y + 10.f}, 
+					{rect.x + 10.f, rect.y + rect.height - 10.f}, 3.f, BLACK
+				);
+				break;
+			case O: 
+				DrawCircleLinesV(
+					{rect.x + SQUARE_SIZE / 2.f, rect.y + SQUARE_SIZE / 2.f}, 
+					SQUARE_SIZE / 2.f - 10.f, BLACK
+				);
+
+				break;
+			default: DrawText(
+				"What the hell did you do", 
+				SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 
+				100, RAYWHITE
+			); break;
+		}
+	}
+}
+
+Player checkWin(std::array<std::array<int, 2>, 3>& winCombination) {
+	// 3 for 3 required squares per win combination, 2 for indices of square in the 2D square array at line 18.
+	// double braces because std::array oh well
+	static std::array<std::array<std::array<int, 2>, 3>, 8> constexpr POSSIBLE_WINS = {{
+		// horizontals
+		{{ { 0, 0 }, { 0, 1 }, { 0, 2 } }},
+		{{ { 1, 0 }, { 1, 1 }, { 1, 2 } }},
+		{{ { 2, 0 }, { 2, 1 }, { 2, 2 } }},
+		// verticals
+		{{ { 0, 0 }, { 1, 0 }, { 2, 0 } }},
+		{{ { 0, 1 }, { 1, 1 }, { 2, 1 } }},
+		{{ { 0, 2 }, { 1, 2 }, { 2, 2 } }},
+		// diagonals
+		{{ { 0, 0 }, { 1, 1 }, { 2, 2 } }},
+		{{ { 0, 2 }, { 1, 1 }, { 2, 0 } }},
+	}};
+
+	Player ret = None;
+	for (auto const& winCondition : POSSIBLE_WINS) {
+		Player marks[3];
+		for (int i = 0; i < 3; i++) marks[i] = squares[winCondition[i][1]][winCondition[i][0]].second;
+		if (marks[0] != None && marks[0] == marks[1] && marks[1] == marks[2]) {
+			ret = marks[0];
+			winCombination = winCondition;
+			break;
+		}
+	}
+
+	return ret;
+}
+
+void initSquares() {
+	for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) {
+		squares[i][j] = std::make_pair(Rectangle{
+			BOARD_START.x + SQUARE_SIZE * j,
+			BOARD_START.y + SQUARE_SIZE * i,
+			SQUARE_SIZE, SQUARE_SIZE
+		}, None);
+	}
+
+}
+
+bool startGame() {
+	
+	initSquares();
+
+	Texture2D endText;
+	
+	bool mouseHeld = false;
+	bool currentTurnIsO = false;
+
+	int turnCount = 0;
+	bool hasEnded = false;
+	bool isWin = false;
+	double endTime = 0.;
+
+	Vector2 lineStartPos;
+	Vector2 lineEndPos;
+	enum { Horizontal, Vertical, Diagonal } winAxis;
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(SKYBLUE);
 
+		if (turnCount == 9 && !hasEnded) {
+			hasEnded = true;
+			endTime = GetTime();
+			auto endTextImage = ImageText(
+				"The game ended in a draw.",
+				50, RED
+			);
+			endText = LoadTextureFromImage(endTextImage);
+		}
+
+		draw();
+
+		if (hasEnded) {
+			bool drawText = true;
+			if (isWin) {
+				float partOfLineToDraw = std::min((GetTime() - endTime) / 2., 1.);
+				if (GetTime() - endTime < 3.) drawText = false;
+
+				Vector2 lineRealEndPos = lineEndPos * partOfLineToDraw; 
+				switch (winAxis) {
+					case Horizontal: lineRealEndPos.y = lineEndPos.y; break;
+					case Vertical:   lineRealEndPos.x = lineEndPos.x; break;
+					case Diagonal:   if (lineEndPos == Vector2{SQUARE_SIZE * 3, 0.f}) lineRealEndPos.y = SQUARE_SIZE * 3 * (1.f - partOfLineToDraw); 
+				}
+				DrawLineEx(BOARD_START + lineStartPos, BOARD_START + lineRealEndPos, 15.f, RED);
+			}
+			DrawTextureV(endText, {SCREEN_WIDTH / 2.f - endText.width / 2.f, 15.f + SQUARE_SIZE * 3 + Y_FREE_SPACE}, WHITE);
+		}
+
+		if (IsMouseButtonDown(0)) mouseHeld = true;
+		else if (!hasEnded) {
+			bool checkForWin = false;
+			if (mouseHeld) for (auto& line : squares) for (auto& [rect, item] : line)
+				if (CheckCollisionPointRec({GetMousePosition()}, rect) && item == None) {
+					item = currentTurnIsO ? O : X;
+					currentTurnIsO = !currentTurnIsO;
+					turnCount++;
+					if (turnCount >= 5) checkForWin = true;
+					break;
+				}
+
+			std::array<std::array<int, 2>, 3> winCombination = {};
+			Player whoWon = None;
+			if (checkForWin) whoWon = checkWin(winCombination);
+			if (whoWon != None) {
+				if (winCombination[0][0] == winCombination[2][0]) { // x's equal, its vertical
+					winAxis = Vertical;
+					lineStartPos = {SQUARE_SIZE * (winCombination[0][0] + .5f), 0.f};
+					lineEndPos   = {SQUARE_SIZE * (winCombination[0][0] + .5f), SQUARE_SIZE * 3};
+				} else if (winCombination[0][1] == winCombination[2][1]) { // y's equal, its horizontal
+					winAxis = Horizontal;
+					lineStartPos = {0.f,             SQUARE_SIZE * (winCombination[0][1] + .5f)};
+					lineEndPos   = {SQUARE_SIZE * 3, SQUARE_SIZE * (winCombination[0][1] + .5f)};
+				} else if (winCombination[0] == std::array<int, 2>{0, 0}) { // not horizontal nor vertical, it must be diagonal
+					winAxis = Diagonal;
+					lineStartPos = {0.f, 0.f};
+					lineEndPos   = {SQUARE_SIZE * 3, SQUARE_SIZE * 3};
+				} else {
+					winAxis = Diagonal;
+					lineStartPos = {0.f, SQUARE_SIZE * 3};
+					lineEndPos   = {SQUARE_SIZE * 3, 0.f};
+				}
+
+				std::cout << "Line start pos: (" << lineStartPos.x << ", " << lineStartPos.y << ")\n"
+						  << "Line end pos:   (" << lineEndPos.x   << ", " << lineEndPos.y   << ")\n";
+
+				hasEnded = true;
+				isWin = true;
+				endTime = GetTime();
+				auto endTextImage = ImageTextEx(GetFontDefault(),
+					std::format("{} has won!", whoWon == X ? "X" : "O").c_str(),
+					50, 1, RED
+				);
+				endText = LoadTextureFromImage(endTextImage);
+			}
+			
+			mouseHeld = false;
+		}
+
 		EndDrawing();
 	}
 
-	CloseWindow();
+	return false;
+}
+
+int main() {
+
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello!");
+	SetTargetFPS(60);
+
+	rlSetLineWidth(5.f);
+
+	while (startGame()) continue;
+
+	// CloseWindow();
 
 	return 0;
 }
