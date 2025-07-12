d5f8ae694b8d0c76affd327b28203d11791d09ea
JohnnyCena123 <iamahuman1395@gmail.com>
2025-07-12 20:57:53 +0300

    debug

--- .github/scripts/build-msi.ps1
+++ .github/scripts/build-msi.ps1
@@ -34,4 +34,6 @@ $wxsContent = @"
 "@
 
 $wxsContent | Set-Content $mainWxs -Encoding UTF8
+Write-Output [DEBUG] main wxs content here:
+Get-Content $mainWxs
 & "C:\Program Files\WiX Toolset v6.0\bin\wix.exe" build -arch x64 -out $OutputMsi $mainWxs $wxsFile
