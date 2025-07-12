781d64e1c46da64bee73c43342ddba5a8947f39d
JohnnyCena123 <iamahuman1395@gmail.com>
2025-07-12 20:41:20 +0300

    script is now a premade file

---
+++ .github/scripts/build-msi.ps1
@@ -0,0 +1,37 @@
+param (
+  [string]$SourceDir,
+  [string]$AppName,
+  [string]$Manufacturer,
+  [string]$Version,
+  [string]$OutputMsi
+)
+
+$wxsFile = "$env:TEMP\$AppName-heat.wxs"
+$mainWxs = "$env:TEMP\$AppName-main.wxs"
+$upgradeCode = [guid]::NewGuid().ToString()
+$targetDirId = "$AppName`Dir"
+
+heat dir $SourceDir -cg AppFiles -dr INSTALLDIR -gg -g1 -srd -sfrag -out $wxsFile
+
+$wxsContent = @"
+<?xml version="1.0" encoding="UTF-8"?>
+<Wix xmlns="http://wixtoolset.org/schemas/v4/wxs">
+  <Package>
+    <Product Name="$AppName" Manufacturer="$Manufacturer" Version="$Version" Language="1033" UpgradeCode="$upgradeCode">
+      <PackageInformation InstallerVersion="500" Compressed="yes" />
+      <Media Id="1" Cabinet="media1.cab" EmbedCab="yes" />
+      <Directory Id="ProgramFilesFolder">
+        <Directory Id="$targetDirId" Name="$AppName">
+          <Directory Id="INSTALLDIR" Name="." />
+        </Directory>
+      </Directory>
+      <Feature Id="DefaultFeature" Title="$AppName Feature" Level="1">
+        <ComponentGroupRef Id="AppFiles" />
+      </Feature>
+    </Product>
+  </Package>
+</Wix>
+"@
+
+$wxsContent | Set-Content $mainWxs -Encoding UTF8
+wix build -arch x64 -out $OutputMsi $mainWxs $wxsFile
