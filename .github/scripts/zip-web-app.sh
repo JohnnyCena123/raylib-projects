b660f6c9109dd793cd4626696732958bf1653568
JohnnyCena123 <iamahuman1395@gmail.com>
2025-08-04 01:53:19 +0300

    add web build job to gha

---
+++ .github/scripts/zip-web-app.sh
@@ -0,0 +1,8 @@
+git_branch="$(git rev-parse --abbrev-ref HEAD)"
+cd "$(dirname "$0")/../../bin/$git_branch" || exit 1
+directory="../../$git_branch-web-app-zip/"
+mkdir -p "$directory"
+cp "$git_branch".{js,wasm,html,data} "$directory"
+cd "$directory" || exit 1
+mv "$git_branch.html" index.html
+zip -r "../$git_branch.zip" "$git_branch".{js,wasm,data} ./index.html
