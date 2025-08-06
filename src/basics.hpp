d0141c8944f37665e51fad77f9f190371e3b8fee
JohnnyCena123 <iamahuman1395@gmail.com>
2025-08-06 17:39:06 +0300

    window is now bigger by default

--- src/basics.hpp
+++ src/basics.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <raylib.h>
 
-inline Vector2 constexpr DEFAULT_SCREEN_SIZE = { 700, 500 };
+inline Vector2 constexpr DEFAULT_SCREEN_SIZE = { 1600, 900 };
 inline Vector2 constexpr DEFAULT_DISPLAY_SIZE = { 1920, 1080 };
 
