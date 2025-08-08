1ff74cc68e03dc8dba1a109249221a17529956b9
JohnnyCena123 <iamahuman1395@gmail.com>
2025-08-08 03:46:55 +0300

    add support for cmake --install and cpack some of the stuff i did here felt too hacky

---
+++ cmake/InjectPackageInfo.cmake
@@ -0,0 +1,31 @@
+if(NOT DEFINED EXE_PATH)
+	message(FATAL_ERROR "Executable path is required.")
+elseif(NOT DEFINED CPACK_PROPERTIES_FILEPATH)
+	message(FATAL_ERROR "CPack Properties filepath is required.")
+endif()
+
+execute_process(
+	COMMAND "${EXE_PATH}" --description
+	OUTPUT_VARIABLE DESCRIPTION
+	OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+execute_process(
+	COMMAND "${EXE_PATH}" --description --minimal-output
+	OUTPUT_VARIABLE MIN_DESCRIPTION
+	OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+string(STRIP "${DESCRIPTION}" DESCRIPTION)                                  # strip out leading/trailing whitespace
+string(STRIP "${MIN_DESCRIPTION}" MIN_DESCRIPTION)                          # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+string(REPLACE "\n" "\\n\n" DESCRIPTION "${DESCRIPTION}")                   # add the newline characters to the description, dont let them decay into spaces (thanks cmake parser)
+string(REPLACE "\n" "\\n\n" MIN_DESCRIPTION "${MIN_DESCRIPTION}")           # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+string(REPLACE "\"" "\\\"" DESCRIPTION "${DESCRIPTION}")                    # properly escape quote characters if exist
+string(REPLACE "\"" "\\\"" MIN_DESCRIPTION "${MIN_DESCRIPTION}")            # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+string(ASCII 27 ESCAPE_CHAR)                                                # theres no other way to produce this specific charcter - all the usual options (\033, \x1b, \e) are unfamiliar to the CMake parser 
+set(REGEXP "${ESCAPE_CHAR}\\[[0-9;?]*[a-zA-Z]")                             # regular expression for ANSI escape codes
+string(REGEX REPLACE "${REGEXP}" "" DESCRIPTION "${DESCRIPTION}")           # strip out all of them
+string(REGEX REPLACE "${REGEXP}" "" MIN_DESCRIPTION "${MIN_DESCRIPTION}")   # ^^^^^^^^^^^^^^^^^^^^^
+file(WRITE "${CPACK_PROPERTIES_FILEPATH}"                                   # write the generated description + summary to the the CPackProperties.cmake file
+    "set(CPACK_PACKAGE_DESCRIPTION \"${DESCRIPTION}\")\n"                   # write the description
+    "set(CPACK_PACKAGE_DESCRIPTION_SUMMARY \"${MIN_DESCRIPTION}\")\n"       # write the summary
+)
