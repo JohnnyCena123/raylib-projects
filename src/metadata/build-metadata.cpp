adae4915e92e5a513b870ab15e8236c738c38c43
JohnnyCena123 <iamahuman1395@gmail.com>
2025-08-09 23:21:36 +0300

    add git commit to metadata

--- src/metadata/build-metadata.cpp
+++ src/metadata/build-metadata.cpp
@@ -1,9 +1,11 @@
 #include "build-metadata.hpp"
 std::string buildMetadata = 
-	"Built for " CMAKE_SYSTEM " (" CMAKE_SYSTEM_NAME ") "
-	"on " __DATE__ "-" __TIME__ " using:\n"
+	"Built for " CMAKE_SYSTEM " (" CMAKE_SYSTEM_NAME ")\n"
+	"at " __TIME__ " on " __DATE__ "\n"
+	"at the git commit hash: " GIT_COMMIT_SHORT " (" GIT_COMMIT ")\n"
+	"using:\n"
 	"  CMake version: " CMAKE_VERSION "\n"
 	"  C Compiler: " C_COMPILER " version " C_COMPILER_VERSION "\n"
 	"  C++ Compiler: " CXX_COMPILER " version " CXX_COMPILER_VERSION "\n"
 	"  C Linker: " C_LINKER " version " C_LINKER_VERSION "\n"
-	"  C++ Linker: " CXX_LINKER" version " CXX_LINKER_VERSION "\n";
+	"  C++ Linker: " CXX_LINKER" version " CXX_LINKER_VERSION;
