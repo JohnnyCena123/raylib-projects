51eb26b30c32d0cc7b92f979bd30b27c8c240dd8
JohnnyCena123 <iamahuman1395@gmail.com>
2025-08-10 19:02:48 +0300

    is this the problem?

--- cmake/InstallConfig.cmake
+++ cmake/InstallConfig.cmake
@@ -75,6 +75,9 @@ else()
 endif()
 set(CPACK_NSIS_MUI_ICON "${CMAKE_SOURCE_DIR}/resources/icon.ico")
 set(CPACK_NSIS_MUI_UNIICON "${CMAKE_SOURCE_DIR}/resources/icon.ico")
+if(WIN32)
+	set(CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/resources/icon.ico")
+endif()
 
 # set(CPACK_BUNDLE_NAME ${CPACK_PACKAGE_NAME})
 
