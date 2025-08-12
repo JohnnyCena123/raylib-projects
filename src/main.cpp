d46a921d5a45fe5ebd13237cd83f0b335a9899ae
JohnnyCena123 <iamahuman1395@gmail.com>
2025-08-12 22:10:53 +0300

    create platform directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,3 @@
-#if defined(NO_TERMINAL_WINDOWS)
-	// holy shit i hate raylib + windows
-	// it compiles so shut up
-	#define _WINGDI_
-	#define LF_FACESIZE 32
-	#define _WINUSER_
-	#define _APISETCONSOLEL3_
-	#define _WINSPOOL_
-	#define _OLE2_H_
-	#define _INC_COMMDLG
-	#define _IMM_SDK_DEFINED_
-	#define _PLAYSOUNDAPI_H_
-	#include <windows.h>
-	int main(int argc, char* argv[]);
-	int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-		// TODO: implement actual commandline arguments handling
-		char buffer[MAX_PATH];
-		(void)GetModuleFileName(NULL, buffer, MAX_PATH);
-		char* argv[] = { buffer };
-		return main(1, argv);
-	}
-#endif
 #include "game.hpp"
 
 int main(int argc, char* argv[]) {
